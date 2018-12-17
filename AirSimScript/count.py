48f4dc019cf2a8260bbecf621c08d5a89099bc11
jinlukang1 <31444152+jinlukang1@users.noreply.github.com>
2018-12-17 17:26:03 +0800

    Add files via upload

--- AirSimScript/count.py
+++ AirSimScript/count.py
@@ -9,6 +9,10 @@ num4_count = []
 num5_count = []
 num6_count = []
 num7_count = []
+num_count = []
+angle_count = []
+distance_count = []
+
 
 
 root1 = "D:/Documents/AirSim/Scene1_Data/daytime/"
@@ -18,32 +22,43 @@ for root in root1,root2,root3:
 	dirnames = os.listdir(root)
 	for dirname in dirnames:
 		realdir = root + dirname
-#	alllist = os.listdir(realdir + "/Annotation/")
-#	for file in alllist:
-#		with open(root+file, "r") as f:
-#			annotation = f.read()
-#			LP_dict = json.loads(annotation)
-#			num1_count.append(LP_dict['license_plate_number']['num1'])
-#			num2_count.append(LP_dict['license_plate_number']['num2'])
-#			num3_count.append(LP_dict['license_plate_number']['num3'])
-#			num4_count.append(LP_dict['license_plate_number']['num4'])
-#			num5_count.append(LP_dict['license_plate_number']['num5'])
-#			num6_count.append(LP_dict['license_plate_number']['num6'])
-#			num7_count.append(LP_dict['license_plate_number']['num7'])
+		alllist = os.listdir(realdir + "/annotations/")
+		for file in alllist:
+			with open(realdir + "/annotations/"+file, "r") as f:
+				annotation = f.read()
+				LP_dict = json.loads(annotation)
+				# num1_count.append(LP_dict['license_plate_number']['num1'])
+				# num2_count.append(LP_dict['license_plate_number']['num2'])
+				# num3_count.append(LP_dict['license_plate_number']['num3'])
+				# num4_count.append(LP_dict['license_plate_number']['num4'])
+				# num5_count.append(LP_dict['license_plate_number']['num5'])
+				# num6_count.append(LP_dict['license_plate_number']['num6'])
+				# num7_count.append(LP_dict['license_plate_number']['num7'])
+				num_count.append(LP_dict['license_plate_number']['num1'])
+				num_count.append(LP_dict['license_plate_number']['num2'])
+				num_count.append(LP_dict['license_plate_number']['num3'])
+				num_count.append(LP_dict['license_plate_number']['num4'])
+				num_count.append(LP_dict['license_plate_number']['num5'])
+				num_count.append(LP_dict['license_plate_number']['num6'])
+				num_count.append(LP_dict['license_plate_number']['num7'])
+				angle_count.append(LP_dict['angle'])
+				distance_count.append(LP_dict['distance'])
 		print(dirname + " is Done!")
-#	for key in LP_dict['license_plate_number']['num1']:
-#		count_dict['num1'] = count_dict.get(key, 0) + 1
 
-	result1 = Counter(num1_count)
-#	result2 = Counter(num2_count)
-#	result3 = Counter(num3_count)
-#	result5 = Counter(num5_count)
-#	result6 = Counter(num6_count)
-#	result7 = Counter(num7_count)
-	print(result1)
-#	print(result2)
-#	print(result3)
-#	print(result4)
-#	print(result5)
-#	print(result6)
-#	print(result7)
+# result1 = Counter(num1_count)
+# result2 = Counter(num2_count)
+# result3 = Counter(num3_count)
+# result4 = Counter(num4_count)
+# result5 = Counter(num5_count)
+# result6 = Counter(num6_count)
+# result7 = Counter(num7_count)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# print(result5)
+# print(result6)
+# print(result7)
+
+result_num = Counter(num_count)
+print(result_num)
