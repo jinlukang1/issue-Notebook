ac447735d0ddfc2f467b35b51d62ae0e8228df54
jinlukang1 <31444152+jinlukang1@users.noreply.github.com>
2019-02-15 20:16:41 +0800

    Add files via upload

---
+++ AirSimScript/img_show.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import json
+from PIL import Image, ImageDraw, ImageFont
+
+
+# DATAROOT = "d:/Documents/AirSim/final_dataset/scene1/noontime_foggy/"
+# DATAROOT = "E:/dataset/final_dataset/front_noontime_lightrain/"
+DATAROOT = "E:/dataset/Scene2_Data/evening/heavy_snow/"
+DATASAVEROOT = "C:/Users/Administrator/Desktop/AirSimData/img/"
+filename = "01555.png"
+
+img = cv2.imread(DATAROOT + "raw/" + filename)
+cv2.namedWindow("images")
+# cv2.imshow("images", img)
+# cv2.waitKey(0)
+img_PIL = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+char_table = {0:"0", 1:"1", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", 10:"A", 11:"B",
+ 12:"C", 13:"D", 14:"E", 15:"F", 16:"G", 17:"H", 18:"J", 19:"K", 20:"L", 21:"M", 22:"N", 23:"P", 24:"Q",
+  25:"R", 26:"S", 27:"T", 28:"U", 29:"V", 30:"W", 31:"X", 32:"Y", 33:"Z", 34:"皖", 35:"桂", 36:"贵", 37:"粤",
+   38:"甘", 39:"京", 40:"冀", 41:"闽", 42:"渝", 43:"琼", 44:"吉", 45:"赣", 46:"豫", 47:"黑", 48:"湘", 49:"苏",
+    50:"辽", 51:"蒙", 52:"宁", 53:"沪", 54:"浙", 55:"青", 56:"鄂", 57:"津", 58:"陕", 59:"新", 60:"鲁", 61:"云",
+     62:"川", 63:"藏", 64:"晋"}
+
+license_plate_content = []
+
+
+
+with open(DATAROOT+"annotations/"+filename[:-3] + "json", "r") as f:
+	annotations = f.read()
+	LP_dict = json.loads(annotations)
+	
+	font = ImageFont.truetype("C:/Users/Administrator/Desktop/songti.ttf", 40)
+	fillColor = (255,255,255)
+	position = (LP_dict['vertices_locations']['x2'], LP_dict['vertices_locations']['y2'])
+
+	license_plate_content.append('license_plate_number:\n')
+	for num in LP_dict['license_plate_number']:
+		license_plate_content.append(char_table[LP_dict['license_plate_number'][num]])
+	license_plate_content.append('\nangle:\n')
+	license_plate_content.append(str(LP_dict['angle']))
+	license_plate_content.append('\ndistance:\n')
+	license_plate_content.append(str(LP_dict['distance']))
+	
+	print(license_plate_content)
+	text = "".join(license_plate_content)
+	if isinstance(text, str):
+		text = text
+		decoded = False
+	else:
+		text = text.decode(encoding)
+		decoded = True
+	draw = ImageDraw.Draw(img_PIL)
+	draw.text(position, text, font=font, fill=fillColor)
+	img_OpenCV = cv2.cvtColor(np.asarray(img_PIL),cv2.COLOR_RGB2BGR)
+
+	cv2.line(img_OpenCV, (LP_dict['vertices_locations']['x1'], LP_dict['vertices_locations']['y1']),
+		(LP_dict['vertices_locations']['x2'], LP_dict['vertices_locations']['y2']), (0,0,255), 2)
+	cv2.line(img_OpenCV, (LP_dict['vertices_locations']['x2'], LP_dict['vertices_locations']['y2']),
+		(LP_dict['vertices_locations']['x3'], LP_dict['vertices_locations']['y3']), (0,0,255), 2)
+	cv2.line(img_OpenCV, (LP_dict['vertices_locations']['x3'], LP_dict['vertices_locations']['y3']),
+		(LP_dict['vertices_locations']['x4'], LP_dict['vertices_locations']['y4']), (0,0,255), 2)
+	cv2.line(img_OpenCV, (LP_dict['vertices_locations']['x4'], LP_dict['vertices_locations']['y4']),
+		(LP_dict['vertices_locations']['x1'], LP_dict['vertices_locations']['y1']), (0,0,255), 2)
+	
+	img_OpenCV = cv2.resize(img_OpenCV,(1280,720),interpolation=cv2.INTER_CUBIC)
+
+	cv2.imshow("images", img_OpenCV)
+	#cv2.imwrite(DATASAVEROOT + filename, img)
+	cv2.waitKey(0)
+
+cv2.destroyAllWindows()
--- AirSimScript/pixel_comp.py
+++ AirSimScript/pixel_comp.py
@@ -1,25 +1,35 @@
+#尝试失败
 import cv2
 import os
 import numpy as np
+import math
+import json
+from collections import Counter
+
 
 PASS_SCORE = 0.02
 
 def Car_Pixel_Num(input_pic):
 	image = cv2.imread(input_pic)
-# 	[rows_suv, cols_suv] = np.where(image[:, :, 2] == 24)
 	[rows, cols] = np.where(image[:, :, 2] == 41)
 	return len(rows)
 
 def Suv_Pixel_Num(input_pic):
 	image = cv2.imread(input_pic)
-# 	[rows_suv, cols_suv] = np.where(image[:, :, 2] == 24)
 	[rows, cols] = np.where(image[:, :, 2] == 24)
 	return len(rows)
 
+def Plate_Pixel_Num(input_pic):
+	image = cv2.imread(input_pic)
+	[rows, cols] = np.where(image[:, :, 2] == 89)
+	return len(rows)
+
 def No_Pass(key_num, current_num):
-	if abs(key_num - current_num)/max(key_num, current_num) < PASS_SCORE:
+	if abs(key_num - current_num) < PASS_SCORE:
+		#print(abs(key_num - current_num)/max(key_num, current_num))
 		return False
 	else:
+		#print(abs(key_num - current_num)/max(key_num, current_num))
 		return True
 
 def Pixel_Num(input_pic):
@@ -27,13 +37,23 @@ def Pixel_Num(input_pic):
 	Suv_Pixel = Suv_Pixel_Num(input_pic)
 	return Suv_Pixel, Car_Pixel
 
+def Angle_Cal(file):
+	with open(file,"r") as f:
+		annotations = f.read()
+		LP_dict = json.loads(annotations)
+		A_x = LP_dict['vertices_locations']['x1']
+		A_y = LP_dict['vertices_locations']['y1']
+		B_x = LP_dict['vertices_locations']['x2']
+		B_y = LP_dict['vertices_locations']['y2']
+	return round((B_y-A_y)/abs(A_x-B_x), 3)
+
+#像素分数以及序号初始化
 def Counter_init(namelist):
 	Car_counter = 0
 	Suv_counter = 0
 	for pic in namelist:
 		pic_path = os.path.join(PATH, pic)
-		Car_Pixel = Car_Pixel_Num(pic_path)
-		Suv_Pixel = Suv_Pixel_Num(pic_path)
+		Suv_Pixel, Car_Pixel = Pixel_Num(pic_path)
 		if Car_Pixel > Suv_Pixel and Car_counter == 0:
 			Car_counter = Car_Pixel
 			print("Car_init_index:", namelist.index(pic))
@@ -45,40 +65,77 @@ def Counter_init(namelist):
 		if Car_counter != 0 and Suv_counter != 0:
 			return Suv_counter, Car_counter
 
+def Car_Type(file):
+	with open(file,"r") as f:
+		annotations = f.read()
+		LP_dict = json.loads(annotations)
+		A_x = LP_dict['vertices_locations']['x1']
+		A_y = LP_dict['vertices_locations']['y1']
+	image = cv2.imread(file.replace("annotations", "segment")[:-4] + "png")
+	return image[A_y-50, A_x, 2]#图像的尺寸是1440*2560
 
+
+car_count = []
 
 # PATH = "C:/Users/Administrator/Desktop/AirSimData/scene/test/segment/"
-PATH = "E:/dataset/final_dataset/scene3_noontime/segment/"
-namelist = os.listdir(PATH)
-print(namelist)
+# PATH = "E:/dataset/final_dataset/front_noontime_lightrain/segment/"
+# PATH = "E:/dataset/final_dataset/scene3_noontime/segment/"
+# PATH = "E:/dataset/Scene2_Data/evening/heavy_snow/segment/"
+camera_count = 1
+
+ANNO_PATH = "E:/dataset/Scene2_Data/evening/heavy_snow/"
+namelist = os.listdir(ANNO_PATH+"annotations/")
 
 
-camera_count = 0
-# Car_pixel_cache = Car_Pixel_Num(os.path.join(path, namelist[0])) 
-# Suv_Cache, Car_Cache = 
-Suv_Cache, Car_Cache = Counter_init(namelist)
-
-for pic in namelist:
-	# print(Car_Pixel_Num(os.path.join(path,pic)))
-	# print(Suv_Pixel_Num(os.path.join(path,pic)))
-	print("Now At:", namelist.index(pic))
-	pic_path = os.path.join(PATH, pic)
-	Suv_Pixel, Car_Pixel = Pixel_Num(pic_path)
-	if No_Pass(Suv_Pixel, Suv_Cache) and No_Pass(Car_Pixel, Car_Cache):
-		camera_count += 1
-		if Suv_Pixel > Car_Pixel:
-			Suv_Cache = Suv_Pixel
-		else:
-			Car_Cache = Car_Pixel
-		print("Camera transfer at:", namelist.index(pic))
+Angle_Cache = Angle_Cal(os.path.join(ANNO_PATH+"annotations/", namelist[0]))
+for pic_anno in namelist:
+	pic_anno_path = os.path.join(ANNO_PATH+"annotations/", pic_anno)
+	if Car_Type(pic_anno_path) == 41:
+		Angle_Score = Angle_Cal(pic_anno_path)
+		# car_count.append(Car_Type(pic_anno_path))
+		if No_Pass(Angle_Score, Angle_Cache):
+			Angle_Cache = Angle_Score
+			camera_count += 1
+			print("Camera transfer at:", namelist.index(pic_anno)+1)
 
 
 
-	
+# Plate_Cache = Plate_Pixel_Num(os.path.join(PATH, namelist[103]))
+# print(Plate_Cache)
+# Plate_Cache = Plate_Pixel_Num(os.path.join(PATH, namelist[99]))
+# print(Plate_Cache)
+
+# Suv_Cache, Car_Cache = Counter_init(namelist)
+# SUV和CAR的像素分数的初始化
+
+# for pic in namelist:
+# 	#print("Now At:", namelist.index(pic)+1)
+# 	pic_path = os.path.join(PATH, pic)
+# 	Suv_Pixel, Car_Pixel = Pixel_Num(pic_path)
+# 	if No_Pass(Suv_Pixel, Suv_Cache) and No_Pass(Car_Pixel, Car_Cache):
+# 		print(abs(Suv_Pixel - Suv_Cache)/max(Suv_Pixel, Suv_Cache))
+# 		print(abs(Car_Pixel - Car_Cache)/max(Car_Pixel, Car_Cache))
+# 		camera_count += 1
+# 		if Suv_Pixel > Car_Pixel:
+# 			Suv_Cache = Suv_Pixel
+# 		else:
+# 			Car_Cache = Car_Pixel
+# 		print("Camera transfer at:", namelist.index(pic)+1)
+
+
+# print(Plate_Cache)
+# for pic in namelist:
+# 	pic_path = os.path.join(PATH, pic)
+# 	Plate_Pixel = Plate_Pixel_Num(pic_path)
+# 	if No_Pass(Plate_Pixel, Plate_Cache):
+# 		print("Camera transfer at:", namelist.index(pic)+1)
+# 		Plate_Cache = Plate_Pixel
+
+
 print(camera_count)
 
 # res=cv2.resize(image1,(1280,720),interpolation=cv2.INTER_CUBIC)
 # cv2.namedWindow("Image") 
-# cv2.imshow("Image", res) 
+# cv2.imshow("Image", res)
 # cv2.waitKey (0)
 # cv2.destroyAllWindows()
